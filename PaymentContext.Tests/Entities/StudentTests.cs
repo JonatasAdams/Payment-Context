97db095f8044c6dfbc755cf19e3a8be46a15f69d
Jonatas Adams da Silva <jonatas.silva@hbsis.com.br>
2020-04-14 17:24:34 -0300

    validations and Flunt

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -9,9 +9,7 @@ namespace PaymentContext.Tests
         [TestMethod]
         public void AdicionarAssinatura()
         {
-          // var subscription = new Subscription(null);
-          // var student = new Student("Jonatas", "Adams", "12345678912", "teste.2020@gmail.com");
-          // student.AddSubscription(subscription);
+          
         }
     }
 }
